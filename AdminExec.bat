fcd0b8169f323b13d3a990478770963a0dd1ba71
Altify <83733589+Z3K3GamingYT@users.noreply.github.com>
2022-02-28 12:53:28 -0800

    Update AdminExec.bat

--- AdminExec.bat
+++ AdminExec.bat
@@ -1,2 +1,11 @@
+@echo off
+title "UAC Admin Executor Bypass Tool By: Altify"
+echo. [32mLoading Information[0m
+echo.
+echo.
+echo.
+echo. [31m [31mEnter Password Below[0m
+SET /P pass=
+if pass==AVKEJtWkBj
 set __COMPAT_LAYER=RunAsInvoker C:/example.exe
 start C:/example.exe
